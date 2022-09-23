9928e41a6be70ddeaef39555ddfa2caa736fc30e
DavidC320 <92796937+DavidC320@users.noreply.github.com>
2022-09-22 21:59:43 -0400

    Brainstrom 2

---
+++ Ally&Enemies.py
@@ -0,0 +1,31 @@
+# 9/192022
+
+class Base_Chacter:
+    def __init__(self):
+        None
+        # Information
+        self.name
+
+        # Statistics
+        self.Health
+        self.speed
+        self.energy
+
+        self.defense
+        self.attack
+
+        # Equipment
+        self.head
+        self.body
+        self.legs
+
+        self.weapon
+        self.pocket
+    
+    @property
+    def Check_equpment(self):
+        None
+
+    @property
+    def attack_roll(self):
+        None
